0f6dbac295615a9b039cbb01e2de1cb0cff6afe3
Janet Zhang <janetzhang321@gmail.com>
2016-10-02 02:57:42 -0400

    finish app!

--- app.py
+++ app.py
@@ -12,23 +12,19 @@ app= Flask(__name__)
 def home() : 
 	#python eqiv of toString, printing an object
 	#User-Agent gives you the info the connection is coming from
-
-
-	#print request.headers 
-	#return render_template( 'form.html', foo="submit form")
-
-
-
     if request.method == 'POST':
-        if request.form['button'] == 'login':
-			print "login"
-            #pass # do something
-        elif request.form['button'] == 'register':
-			print "register"
-            #pass # do something else
-        else:
-			print "unknown"
-            #pass # unknown
+		print "name: " + request.form['button']
+        # if request.form['button'] == 'login':
+		# 	pass
+		# 	print "login"
+        #     #pass # do something
+        # elif request.form['button'] == 'register':
+		# 	print "register"
+        #     #pass # do something else
+        # else:
+		# 	print "unknown"
+        #     #pass # unknown
+		return render_template('form.html', foo="submit form")
     elif request.method == 'GET':
         return render_template('form.html', foo="submit form")
 
@@ -37,11 +33,17 @@ def home() :
 
 @app.route("/auth/", methods = ['POST'])
 def auth() :
+	print "name from auth: " + request.form['button']
+	if request.form['button'] == "login":
+		return render_template( 'form.html', poo=register.login(request.form['user'],request.form['pass']) )
 	return render_template( 'form.html', foo="account", poo=register.addUser(request.form['user'],request.form['pass']) )
 
 
 @app.route("/login/", methods = ['POST'])
 def login() :
+	print "name from login: " + request.form['button']
+	if request.form['button'] == "register":
+		return render_template( 'form.html', foo="account", poo=register.addUser(request.form['user'],request.form['pass']) )
 	return render_template( 'form.html', poo=register.login(request.form['user'],request.form['pass']) )
 
 
