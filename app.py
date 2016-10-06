199163fa4681187d726e24d4482a856ca7b55aeb
Janet Zhang <jzhang12@stuy.edu>
2016-10-06 12:18:45 -0400

    update

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 #Janet Zhang
 #Pd 6 SoftDev DW
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
 from utils import register
 import cgi
 
 app= Flask(__name__)
+app.secret_key = "g?\x01l\x1e\xf7\xb6\xdc\x0b:\x83\xf9O\x92\xeb\xcc\xd7\x02Z+\xd4uWM\x06'\x94\xbei\xf5\xeb\xda"
+
 
 @app.route("/")
 @app.route("/home/", methods = ['POST'])
@@ -13,18 +15,8 @@ def home() :
 	#python eqiv of toString, printing an object
 	#User-Agent gives you the info the connection is coming from
     if request.method == 'POST':
-		print "name: " + request.form['button']
-        # if request.form['button'] == 'login':
-		# 	pass
-		# 	print "login"
-        #     #pass # do something
-        # elif request.form['button'] == 'register':
-		# 	print "register"
-        #     #pass # do something else
-        # else:
-		# 	print "unknown"
-        #     #pass # unknown
-		return render_template('form.html', foo="submit form")
+			print "name: " + request.form['button']
+			return render_template('form.html', foo="submit form")
     elif request.method == 'GET':
         return render_template('form.html', foo="submit form")
 
@@ -42,6 +34,9 @@ def auth() :
 @app.route("/login/", methods = ['POST'])
 def login() :
 	print "name from login: " + request.form['button']
+	print url_for("login")
+	#the correct route of the function in your flask app
+	print url_for("auth")
 	if request.form['button'] == "register":
 		return render_template( 'form.html', foo="account", poo=register.addUser(request.form['user'],request.form['pass']) )
 	return render_template( 'form.html', poo=register.login(request.form['user'],request.form['pass']) )
